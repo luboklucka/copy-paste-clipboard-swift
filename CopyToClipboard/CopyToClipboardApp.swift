b6e098b0a68253d7a6142ea28c976aed15da6800
Lubomir Klucka <lubo.klucka@gmail.com>
2025-08-11 09:49:35 +0200

    Initial Commit

---
+++ CopyToClipboard/CopyToClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyToClipboardApp.swift
+//  CopyToClipboard
+//
+//  Created by Lubo Klucka on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CopyToClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
